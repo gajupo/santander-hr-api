685521563624ed2f058465e67ac61195f1eaceaa
Gaudencio Jurado <gjurado@virtualidevs.com>
2025-02-04 10:57:21 -0600

    implement global exception handler for improved error management

--- Extensions/ApplicationServiceConfig.cs
+++ Extensions/ApplicationServiceConfig.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Serilog;
 
 namespace santander_hr_api.Extensions
@@ -13,6 +15,35 @@ namespace santander_hr_api.Extensions
                         .ReadFrom.Services(services)
                         .Enrich.FromLogContext()
                         .WriteTo.Console()
+            );
+        }
+
+        public static void UseGlobalExceptionHandler(this WebApplication app)
+        {
+            // global unhlandel exception in case some controller or action does not handle some error
+            app.UseExceptionHandler(a =>
+                a.Run(async context =>
+                {
+                    var exceptionHandlerPathFeature =
+                        context.Features.Get<IExceptionHandlerPathFeature>();
+                    var exception = exceptionHandlerPathFeature?.Error;
+
+                    Log.Error(exception, "An unhandled exception has occurred");
+
+                    ProblemDetails problemDetails =
+                        new()
+                        {
+                            Title = exception?.Message,
+                            Status = StatusCodes.Status500InternalServerError,
+                            Detail = exception?.StackTrace,
+                            Type = "GlobalExceptionHandler"
+                        };
+
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/problem+json";
+
+                    await context.Response.WriteAsJsonAsync(problemDetails);
+                })
             );
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using santander_hr_api.Extensions;
 using Serilog;
 
@@ -53,31 +51,8 @@ namespace santander_hr_api
                 // build the application
                 await using WebApplication app = builder.Build();
 
-                // global unhlandel exception in case some controller or action does not handle some error
-                app.UseExceptionHandler(a =>
-                    a.Run(async context =>
-                    {
-                        var exceptionHandlerPathFeature =
-                            context.Features.Get<IExceptionHandlerPathFeature>();
-                        var exception = exceptionHandlerPathFeature?.Error;
-
-                        Log.Error(exception, "An unhandled exception has occurred");
-
-                        ProblemDetails problemDetails =
-                            new()
-                            {
-                                Title = exception?.Message,
-                                Status = StatusCodes.Status500InternalServerError,
-                                Detail = exception?.StackTrace,
-                                Type = "GlobalExceptionHandler"
-                            };
-
-                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                        context.Response.ContentType = "application/problem+json";
-
-                        await context.Response.WriteAsJsonAsync(problemDetails);
-                    })
-                );
+                // configure the global exception handler
+                app.UseGlobalExceptionHandler();
 
                 app.UseHttpsRedirection();
 
